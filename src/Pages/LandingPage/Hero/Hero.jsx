cc1486b20af1f4b0b0e13b1a3fa6f7d8a3d28dfb
Ahmadsultan395 <ahmad395sultan@gmail.com>
2024-09-01 12:16:44 -0700

    set background image

---
+++ src/Pages/LandingPage/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+
+const Hero = () => {
+  return (
+    <div className='bg-[url("/bg3.png")] w-full h-[100vh] bg-cover bg-no-repeat bg-center '>
+        <div>
+            <Navbar/>
+        </div>
+        <div >
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero

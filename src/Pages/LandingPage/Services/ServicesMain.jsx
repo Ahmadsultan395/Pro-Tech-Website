3c76f1314c49393052720ef0663d5e04cfe40635
Ahmadsultan395 <ahmad395sultan@gmail.com>
2024-09-03 04:13:36 -0700

    set width

--- src/Pages/LandingPage/Services/ServicesMain.jsx
+++ src/Pages/LandingPage/Services/ServicesMain.jsx
@@ -26,9 +26,9 @@ const ServicesMain = () => {
             We offer cutting-edge software solutions, from web development and mobile apps to cloud services and digital marketing.
             </p>
         </div>
-        <div className='flex flex-col md:flex-row justify-center gap-3 md:gap-3 '>
+        <div className='flex flex-col md:flex-row justify-center gap-3 md:gap-3  mt-3 md:mt-0'>
             {listData.map((row, index)=>(
-                <div key={index} className='md:max-w-sm border-[1px] border-gray-700 p-5 rounded-xl mt-0 md:mt-10 
+                <div key={index} className='md:max-w-full border-[1px] border-gray-700 p-5 rounded-xl mt-0 md:mt-10 
                  cursor-pointer  hover:bg-[#db8a3b] group transition duration-300'>
                 <h1 className='text-xl md:text-3xl mb-2 text-[#db8a3b] group-hover:text-white transition duration-300'>
                     {row.icon}
